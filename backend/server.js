a9fc3a8ffb7f77afc62905b558df4d0d2e49c603
Sneha <janhvi9415@gmail.com>
2024-12-02 13:07:39 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,24 @@ import {app, server} from './socket/socket.js';
 
 import messageRoutes from './routes/messageRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import path from 'path';
 
 const PORT = process.env.PORT;
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// })
+const __dirname = path.resolve();
+
 app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
 app.use("/api/messages", messageRoutes);
 app.use('/api/users', userRoutes);
+
+app.use(express.static(path.join(__dirname, 'frontend/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/dist/index.html'))
+})
 server.listen(PORT, () => {
   connectDB();
   console.log('Server started on port ' + PORT);
